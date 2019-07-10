19ac993a8d999d7e274b9704c5544889f7b33977
Prem Shaganti <prem.shaganti@dc.gov>
2019-07-10 15:39:40 -0400

    Ref:42047 DRY for CSV headers

--- components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller_spec.rb
+++ components/benefit_sponsors/spec/controllers/benefit_sponsors/profiles/employers/employer_profiles_controller_spec.rb
@@ -9,7 +9,7 @@ module BenefitSponsors
     let(:person) { FactoryBot.create(:person) }
     let(:user) { FactoryBot.create(:user, :person => person)}
 
-    let!(:site)                  { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, :cca) }
+    let!(:site)                  { create(:benefit_sponsors_site, :with_benefit_market, :as_hbx_profile, site_key: Settings.site.key) }
     let!(:benefit_sponsor)       { FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site) }
     let(:employer_profile)      { benefit_sponsor.employer_profile }
     let!(:rating_area)           { FactoryBot.create_default :benefit_markets_locations_rating_area }
@@ -85,7 +85,7 @@ module BenefitSponsors
       let!(:employee) do
         FactoryBot.create(:census_employee, employer_profile: employer_profile, benefit_sponsorship: benefit_sponsorship)
       end
-      
+
       it "should render the view successfully with content as CSV" do
         benefit_sponsorship.save!
         sign_in user
